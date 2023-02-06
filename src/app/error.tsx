0f8ee92019dbee597d2a075c8854e7e254be4a9d
Mirza Shahan Baig <shahan.baig@cooperativecomputing.com>
2023-02-06 20:09:10 +0500

    getStartedChakra

---
+++ src/app/error.tsx
@@ -0,0 +1,4 @@
+"use client";
+export default function error(errors: any) {
+  return <p>error {JSON.stringify(errors)}</p>;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,22 @@
-import './globals.css'
+"use client";
+
+import "./globals.css";
+import { ChakraBaseProvider, extendBaseTheme } from "@chakra-ui/react";
+// `@chakra-ui/theme` is a part of the base install with `@chakra-ui/react`
+import chakraTheme from "@chakra-ui/theme";
+
+const { Button } = chakraTheme.components;
+
+const theme = extendBaseTheme({
+  components: {
+    Button,
+  },
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +25,9 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <ChakraBaseProvider theme={theme}>{children}</ChakraBaseProvider>
+      </body>
     </html>
-  )
+  );
 }
